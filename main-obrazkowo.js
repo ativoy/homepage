624675c618cd96cb34eaed4f7fc5ef4302dfb713
Jowita Hermanowska <jowita.herm@gmail.com>
2020-05-10 19:55:28 +0200

    initial commit

---
+++ main-obrazkowo.js
@@ -0,0 +1,4 @@
+alert('Ahoj z lockdown!');
+const name='Jowita';
+const color='violet';
+console.log(`To jest mix fotek od dziewczyny o imieniu ${name}, której ulubionym kolorem jest ${color}`);
---
+++ main.js
@@ -0,0 +1,5 @@
+const name = 'Jowita';
+const age = 37;
+console.log(`Nazywam się ${name}`);
+console.log(`Mam ${age}`);
+alert('Cześć Jowi jak się masz?');
